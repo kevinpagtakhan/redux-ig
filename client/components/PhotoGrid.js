732aab3dbf5e34a41e7ca35e0253b583323f9091
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-30 22:17:15 -0800

    PhotoGrid component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import PhotoGridItem from './PhotoGridItem';
+
 const PhotoGrid = React.createClass({
   render() {
+
+    var photos = this.props.posts.map((post, index) => (
+      <PhotoGridItem
+        { ...this.props }
+        index={index}
+        key={index}
+        post={post}
+      />
+    ));
+
     return (
       <div className='photo-grid'>
-        Photo grid
+        { photos }
       </div>
     )
   }
---
+++ client/components/PhotoGridItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGridItem = React.createClass({
+  render() {
+    return (
+      <figure className='grid-figure'>
+        I am a photo
+      </figure>
+    )
+  }
+});
+
+export default PhotoGridItem;
