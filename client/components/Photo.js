c3384fcb4536444f52536dd8bbe37064d8bc27fb
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-30 18:08:36 -0800

    Test Photo and Grip components

---
+++ client/components/Photo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div class='photo'>
+        Photo
+      </div>
+    )
+  }
+});
+
+export default Main;
---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div class='photo-grid'>
+        Photo grid
+      </div>
+    )
+  }
+});
+
+export default Main;
